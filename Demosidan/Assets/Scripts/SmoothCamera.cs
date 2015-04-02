d32d61f740fda9dc902b3e726e5ff0796220bc32
Emil Dannberger <ed222an@student.lnu.se>
2015-04-02 17:31:29 +0200

    Camera prefab & follow fix
    Fixed the camera follow, also added the main camera to the prefabs
    folder.

--- Demosidan/Assets/Scripts/SmoothCamera.cs
+++ Demosidan/Assets/Scripts/SmoothCamera.cs
@@ -6,12 +6,12 @@ public class SmoothCamera : MonoBehaviour
     public float dampTime = 0.15f;
     public Transform target;
 
-    private Camera camera;
+    private Camera mainCamera;
     private Vector3 velocity = Vector3.zero;
 
     void Start()
     {
-        camera = GetComponent<Camera>();
+		mainCamera = GetComponent<Camera>();
     }
 
 	// Update is called once per frame
@@ -19,8 +19,8 @@ public class SmoothCamera : MonoBehaviour
     {
         if (target)
         {
-            Vector3 point = camera.WorldToViewportPoint(target.position);
-            Vector3 delta = target.position - camera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z));
+			Vector3 point = mainCamera.WorldToViewportPoint(target.position);
+			Vector3 delta = target.position - mainCamera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z));
             Vector3 destination = transform.position + delta;
             transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
         }   
