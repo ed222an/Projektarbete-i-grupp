79c058e3aae8c7985d4ecbebc190ae488de9488f
hk222gn <hk222gn@student.lnu.se>
2015-04-03 13:53:21 +0200

    Started on an enemy

---
+++ Demosidan/Assets/Scripts/EnemyHandler.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyHandler : MonoBehaviour 
+{
+    public float followRange;
+    public Transform target;
+
+	// Use this for initialization
+	void Start() 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update() 
+    {
+	
+	}
+}
