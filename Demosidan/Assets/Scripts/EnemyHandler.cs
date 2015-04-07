2802529e4070d76702cb1cadbdb4a10bab3b3006
hk222gn <hk222gn@student.lnu.se>
2015-04-07 17:07:03 +0200

    More work on enemy 1

--- Demosidan/Assets/Scripts/EnemyHandler.cs
+++ Demosidan/Assets/Scripts/EnemyHandler.cs
@@ -5,16 +5,28 @@ public class EnemyHandler : MonoBehaviour
 {
     public float followRange;
     public Transform target;
+    public float speed;
 
+    private Rigidbody2D rBody;
 	// Use this for initialization
 	void Start() 
     {
-	
+        rBody = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
-	void Update() 
+	void FixedUpdate() 
     {
-	
+        if (transform != null && (transform.position.x + followRange > target.position.x && transform.position.x < target.position.x) 
+            || (transform.position.x - followRange < target.position.x && transform.position.x > target.position.x))
+            FollowTarget();
 	}
+
+    void FollowTarget()
+    {
+        if (transform.position.x < target.position.x)
+            rBody.velocity = new Vector2(1f * speed, rBody.velocity.y);
+        else
+            rBody.velocity = new Vector2(-1f * speed, rBody.velocity.y);
+    }
 }
