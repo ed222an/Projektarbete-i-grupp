bcaa72a07b450aa240bebba805354ca74b26a92e
Emil Dannberger <ed222an@student.lnu.se>
2015-04-20 17:43:54 +0200

    Workerbot prefab & animations
    Created a workerbot prefab with idle and running animation. Also made
    some code adjustments.

--- Demosidan/Assets/Scripts/EnemyScripts/EnemyHandler.cs
+++ Demosidan/Assets/Scripts/EnemyScripts/EnemyHandler.cs
@@ -8,14 +8,17 @@ public class EnemyHandler : MonoBehaviour
     public Transform target;
     public float speed;
     public LayerMask playerLayer;
+	public bool facingRight = true;
 
     private Rigidbody2D rBody;
-
+	private Animator anim;
     private EnemyStats enemyStats;
+
 	// Use this for initialization
 	void Start() 
     {
         rBody = GetComponent<Rigidbody2D>();
+		anim = GetComponent<Animator>();
         enemyStats = GetComponent<EnemyStats>();
 	}
 	
@@ -27,16 +30,49 @@ public class EnemyHandler : MonoBehaviour
                                                 new Vector2(transform.position.x + followRangeRadiusX, transform.position.y + followRangeRadiusY), playerLayer);
 
         if (target != null && isInRange)
+		{
             FollowTarget();
+		}
+		else
+		{
+			anim.SetBool("Moving", false);
+		}
     }
 
     void FollowTarget()
     {
         if (transform.position.x < target.position.x)
+		{
+			if(!facingRight)
+			{
+				Flip();
+			}
+
+			facingRight = true;
             rBody.velocity = new Vector2(1f * speed, rBody.velocity.y);
+		}
         else
+		{
+			if(facingRight)
+			{
+				Flip();
+			}
+
+			facingRight = false;
             rBody.velocity = new Vector2(-1f * speed, rBody.velocity.y);
+		}
+
+		anim.SetBool("Moving", true);
     }
+
+	// Flips the world around the enemy, allowing us to only use 1 set of animations.
+	void Flip()
+	{
+		facingRight = !facingRight;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
 
     public void KnockbackOnHit(float playerPosX, float enemyPosX)
     {
