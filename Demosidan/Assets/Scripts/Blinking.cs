1f671a7723f9304acf2c42d76787e6d80249297b
Emil Dannberger <ed222an@student.lnu.se>
2015-04-23 14:47:15 +0200

    Added scenery to basement
    Created some new ambiance objects to the basement along with a blinking
    script.

---
+++ Demosidan/Assets/Scripts/Blinking.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Blinking : MonoBehaviour
+{
+	public float blinkDelay;
+
+	private SpriteRenderer objRenderer;
+
+	// Use this for initialization
+	void Start ()
+	{
+		objRenderer = gameObject.GetComponent<SpriteRenderer>();
+		StartCoroutine(Blink());
+	}
+
+	private IEnumerator Blink()
+	{
+		while(true)
+		{
+			objRenderer.enabled = false;
+			yield return new WaitForSeconds(blinkDelay);
+
+			objRenderer.enabled = true;
+			yield return new WaitForSeconds(blinkDelay);
+		}
+	}
+}
