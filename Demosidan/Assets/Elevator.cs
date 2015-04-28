42863e698572d0133195b0ccdd29b9944c655cbd
Emil Dannberger <ed222an@student.lnu.se>
2015-04-28 16:24:35 +0200

    Mine objects
    Added an elevator and a couple of platforms to the mine level.

---
+++ Demosidan/Assets/Elevator.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Elevator : MonoBehaviour
+{
+	public Transform startPos;
+	public Transform endPos;
+
+	public float moveSpeed = 2.0f;
+
+	// Use this for initialization
+	void Start ()
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+		/*
+		transform.Translate(Vector2.up * moveSpeed * Time.deltaTime);
+
+		if(transform.position.y >= startPos.position.y)
+		{
+			Debug.Log ("down");
+			moveSpeed = moveSpeed * -1;
+		}
+
+		if (transform.position.y <= endPos.position.y)
+		{
+			Debug.Log ("up");
+			moveSpeed = moveSpeed * -1;
+		}
+
+		*/
+	}
+}
