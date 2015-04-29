700b91134eb8861a399ff48fe83869ab4a9f28f4
km222ew <km222ew@student.lnu.se>
2015-04-29 18:20:57 +0200

    Level select functioning
    Choose level at the map in the house, hover over the places and click to
    choose. Go back to the house at mine by moving left from where you
    spawn.

---
+++ Demosidan/Assets/Scripts/TriggerScripts/TeleportHome.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TeleportHome : MonoBehaviour {
+
+    private GameObject controller;
+    public int sceneToLoad;
+
+    void Awake()
+    {
+        controller = GameObject.Find("GameController");
+
+    }
+
+    IEnumerator ChangeLevel()
+    {
+        float fadeTime = controller.GetComponent<Fading>().BeginFade(1);
+        yield return new WaitForSeconds(fadeTime);
+        GameController.characterStatsMenuActive = false;
+        Application.LoadLevel(sceneToLoad);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            StartCoroutine(ChangeLevel());
+            Debug.Log("Enter " + this.GetType().Name + " on object " + gameObject.name);
+        }
+    }
+}
