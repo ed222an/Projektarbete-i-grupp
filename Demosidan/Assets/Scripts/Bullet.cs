314447ba1bc6fbf59401da2627cbb31559b9d0eb
Emil Dannberger <ed222an@student.lnu.se>
2015-05-18 13:50:55 +0200

    Bullet changes
    Edited the bullet script.

---
+++ Demosidan/Assets/Scripts/Bullet.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour
+{
+	public float speed = 100;
+
+	private GameObject player;
+	private Rigidbody2D rb;
+	private bool addSpeed = true;
+	
+	void Start ()
+	{
+		player = GameObject.FindGameObjectWithTag ("Player");
+		rb = gameObject.GetComponent<Rigidbody2D>();
+	}
+
+	void FixedUpdate ()
+	{
+		if(addSpeed)
+		{
+			addSpeed = false;
+
+			if (player.transform.position.x <= transform.position.x)
+			{
+				rb.AddForce(transform.right * speed);
+				Debug.Log ("Shoot right!");
+			}
+			else
+			{
+				rb.AddForce(transform.right * -speed);
+				Debug.Log("Shoot left!");
+			}
+
+			StartCoroutine(Despawn());
+		}
+	}
+
+	private IEnumerator Despawn()
+	{
+		yield return new WaitForSeconds (0.5f);
+		Destroy (gameObject);
+	}
+}
